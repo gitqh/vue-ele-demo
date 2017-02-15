420960225693f44b4b708ff7af65ad2452c89d95
qh <quhang19881208@163.com>
2017-02-15 16:42:01 +0800

    Rejected commit: Default

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="(item,index) in goods" class="menu-item" :class="{'current':currentIndex===index}"
+        @click="selectMenu(index,$event)">
           <span class="text border-1px">
             <spanicon iconSize="3" :index="item.type" v-show="item.type>0"></spanicon>{{item.name}}
           </span>
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item">
@@ -26,8 +27,8 @@
                   <span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span class="now">¥{{food.price}}</span>
-                  <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+                  <span class="now">¥{{food.price}}</span><span class="old"
+                                                                v-show="food.oldPrice">¥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -56,12 +57,20 @@
         width: 56px
         line-height: 14px
         padding: 0 12px
+        &.current
+          z-index: 10
+          position: relative
+          margin-top: -1px
+          background: #fff
+          font-weight:700
+          .text
+            border-none()
         .text
           display: table-cell
           width: 56px
           vertical-align: middle
           font-size: 12px
-          border-1px(rgba(7,17,27,0.1))
+          border-1px(rgba(7, 17, 27, 0.1))
     .foods-wrapper
       flex: 1
       .title
@@ -69,13 +78,13 @@
         height: 26px
         line-height: 26px
         border-left: 2px solid #d9dde1
-        color: rgb(147,153,159)
+        color: rgb(147, 153, 159)
         background: #f3f5f7
       .food-item
         display: flex
         margin: 18px
         padding-bottom: 18px
-        border-1px(rgba(7,17,27,0.1))
+        border-1px(rgba(7, 17, 27, 0.1))
         &:last-child
           border-none()
           margin-bottom: 0
@@ -89,15 +98,16 @@
             height: 14px
             line-height: 14px
             font-size: 14px
-            color: rgb(7,17,27)
-          .desc,extra
+            color: rgb(7, 17, 27)
+          .desc, .extra
             line-height: 10px
             font-size: 10px
-            color: rgb(147,153,159)
+            color: rgb(147, 153, 159)
           .desc
             margin-bottom: 8px
+            line-height: 12px;
           .extra
-            &.count
+            .count
               margin-right: 12px
           .price
             font-weight: 700
@@ -105,14 +115,15 @@
             .now
               margin-right: 18px
               font-size: 10px
-              color: rgb(240,20,20)
+              color: rgb(240, 20, 20)
             .old
               text-decoration: line-through
               font-size: 10px
-              color: rgb(147,153,159)
+              color: rgb(147, 153, 159)
 </style>
 <script type='text/ecmascript-6'>
   import spanicon from '../spanicon/spanicon.vue';
+  import BScroll from 'better-scroll';
   const ERR_OK = 0;
   export default {
     props: {
@@ -123,17 +134,67 @@
     components: {
       spanicon
     },
+    computed: {
+      currentIndex() {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+            return i;
+          }
+        }
+        return 0;
+      }
+    },
+    methods: {
+      _initScroll() {
+        this.meunScroll = new BScroll(this.$refs.menuWrapper, {
+          click: true
+        });
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+          click: true,
+          probeType: 3
+        });
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        });
+      },
+      _caculateHeight() {
+        let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+        let height = 0;
+        this.listHeight.push(height);
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i];
+          height += item.clientHeight;
+          this.listHeight.push(height);
+        }
+      },
+      selectMenu(index, event) {
+        if (!event._constructed) {
+          return;
+        }
+        let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+        let el = foodList[index];
+        this.foodsScroll.scrollToElement(el, 300);
+      }
+    },
     created() {
       this.$http.get('/api/goods').then((response) => {
         response = response.body;
         if (response.errno === ERR_OK) {
           this.goods = response.data;
+          this.$nextTick(() => {
+            this._initScroll();
+            this._caculateHeight();
+          });
         }
       });
     },
     data() {
       return {
-        goods: []
+        goods: [],
+        listHeight: [],
+        scrollY: 0
       };
     }
   };
