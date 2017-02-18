9b6fbc4cc83720cdb7c90203cd5299e529c8db57
qh <quhang19881208@163.com>
2017-02-19 00:43:45 +0800

    Rejected commit: Default

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -15,15 +15,27 @@
             <span class="rating">好评率{{food.rating}}%</span>
           </div>
           <price v-bind:nowPrice="food.price" v-bind:oldPrice="food.oldPrice?food.oldPrice:0"></price>
-        </div>
-        <div class="cartcontrol-wrapper">
-          <cartcontrol :food="food"></cartcontrol>
-        </div>
-        <transition name="fade">
-          <div class="buy" v-show="!food.count || food.count===0" @click.stop.prevent="addFirst">
-            加入购物车
+          <div class="cartcontrol-wrapper">
+            <cartcontrol :food="food"></cartcontrol>
           </div>
-        </transition>
+          <transition name="fade">
+            <div class="buy" v-show="!food.count || food.count===0" @click.stop.prevent="addFirst">
+              加入购物车
+            </div>
+          </transition>
+        </div>
+        <split v-show="food.info"></split>
+        <div class="info" v-show="food.info">
+          <h1 class="title">商品信息</h1>
+          <p class="text">{{food.info}}</p>
+        </div>
+        <split></split>
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+          <ratingselect :select-type="selectType"
+                        :only-content="onlyContent" :desc="desc"
+                        :ratings="food.ratings"></ratingselect>
+        </div>
       </div>
     </div>
   </transition>
@@ -65,52 +77,78 @@
           color: #fff
 
     .content
+      position: relative
       padding: 18px
       .title
         line-height: 14px
         margin-bottom: 8px
         font-size: 14px
         font-weight: 700
-        color: rgb(7,17,27)
+        color: rgb(7, 17, 27)
       .detail
         margin-bottom: 18px
-        line-height:10px
+        line-height: 10px
         font-size: 0
         height: 10px
         .sell-count, .rating
           font-size: 10px
-          color: rgb(147,153,159)
+          color: rgb(147, 153, 159)
         .sell-count
           margin-right: 12px
-    .cartcontrol-wrapper
-      position: absolute
-      right: 12px
-      bottom: 12px
-    .buy
-      position: absolute
-      right: 18px
-      bottom: 18px
-      z-index: 10
-      height: 24px
-      line-height: 24px
-      padding: 0 12px
-      boxsizing: border-box
-      font-size: 10px
-      border-radius: 12px
-      color: #fff
-      background: rgb(0,160,220)
-      opacity: 1
-      &.fade-enter, &.fade-leave-active
-        opacity: 0
-        z-index: -1
-      &.fade-enter-active, &.fade-leave-active
-        transition: all 0.2s
+      .cartcontrol-wrapper
+        position: absolute
+        right: 12px
+        bottom: 12px
+      .buy
+        position: absolute
+        right: 18px
+        bottom: 18px
+        z-index: 10
+        height: 24px
+        line-height: 24px
+        padding: 0 12px
+        boxsizing: border-box
+        font-size: 10px
+        border-radius: 12px
+        color: #fff
+        background: rgb(0, 160, 220)
+        opacity: 1
+        &.fade-enter, &.fade-leave-active
+          opacity: 0
+          z-index: -1
+        &.fade-enter-active, &.fade-leave-active
+          transition: all 0.2s
+    .info
+      padding: 18px
+      .title
+        margin-bottom: 6px
+        font-size: 14px
+        line-height: 14px
+      .text
+        font-size: 12px
+        line-height: 24px
+        font-weight: 200
+        color: rgb(77, 85, 93)
+        padding: 0 8px
+    .rating
+      padding-top: 18px
+      .title
+        margin-left: 18px
+        font-size: 14px
+        line-height: 14px
+        color: rgb(7, 17, 27)
 </style>
 <script type='text/ecmascript-6'>
   import price from '../price/price.vue';
   import BScroll from 'better-scroll';
   import cartcontrol from '../cartcontrol/cartcontrol.vue';
   import Vue from 'vue';
+  import split from '../split/split.vue';
+  import ratingselect from '../ratingselect/ratingselect.vue';
+
+  //  const POSITIVE = 0;
+  //  const NEGATIVE = 1;
+  const ALL = 2;
 
   export default {
     props: {
@@ -118,17 +156,23 @@
         type: Object
       }
     },
-    created() {
-      console.log(this.food.oldPrice);
-    },
     data() {
       return {
-        showFlag: false
+        showFlag: false,
+        selectType: ALL,
+        onlyContent: true,
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
+        }
       };
     },
     methods: {
       show() {
         this.showFlag = true;
+        this.selectType = ALL;
+        this.onlyContent = true;
         this.$nextTick(() => {
           if (!this.scroll) {
             this.scroll = new BScroll(this.$refs.food, {
@@ -152,7 +196,9 @@
     },
     components: {
       price,
-      cartcontrol
+      cartcontrol,
+      split,
+      ratingselect
     }
   };
 </script>
